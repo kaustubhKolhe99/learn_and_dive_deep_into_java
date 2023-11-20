64e37c4edf5bba75fc8362002a95b7331448bcbb
kaustubhKolhe99 <142173158+kaustubhKolhe99@users.noreply.github.com>
2023-11-20 15:14:10 +0530

    hashMap.java file added

---
+++ CollectionFramework/src/CollectionFramework/hashMap.java
@@ -0,0 +1,39 @@
+package CollectionFramework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class hashMap {
+    public static void main(String[] args) {
+        Map<Integer , String> students = new java.util.HashMap<>();
+
+        //students.put(1,"John");
+        students.put(2,"tom"); //added entry
+        students.put(2, "Tom Doe"); // updated entry
+
+
+        Map<Integer , String> otherMap = new HashMap<>(Map.of(2,"Samantha" , 4 , "Olivia"));
+
+        students.putAll(otherMap);
+        students.putIfAbsent(1,"Ryan"); //updates only if the key is absent
+        students.computeIfAbsent(5 , k->{
+          return "Oggy";
+        } );
+
+        students.computeIfPresent(1, (k,v) -> v.toUpperCase() +"!");
+        students.compute(2 , (k , v) -> v.toUpperCase()+"!");
+        students.compute(3 , (k , v) -> {
+            if(students.containsKey(k)){
+                return v.toUpperCase() +"!" ;
+
+            }
+            return "Jonny";
+        });
+        System.out.println(students);
+
+
+
+
+
+    }
+}
